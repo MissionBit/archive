4b3dd6b5a10bf4d512b7bccdf1b34ead20ae33a8
Allan Lei <allanlei@gmail.com>
2013-11-14 17:51:34 -0800

    append input to output ventana

--- www/js/chat.js
+++ www/js/chat.js
@@ -2,4 +2,11 @@ function send (){
 	var inputTextArea= document.getElementById("input");
 	var inputText= inputTextArea.value;
 	alert (inputText);
+	appendOutput(inputText);
+}
+function appendOutput (text) {
+	var outputTextArea= document.getElementById("output");
+	var oldText= outputTextArea.value;
+	var newText= oldText+text;
+	outputTextArea.value= newText;
 }
