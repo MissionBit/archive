e0647bd7301776d884400945404ce1cfc7948682
Kevin Chard <krchard@gmail.com>
2013-12-11 00:08:34 +0000

    adding javascript for chatlist

---
+++ www/js/list.js
@@ -0,0 +1,33 @@
+var clearList = function(list) {
+    for(i=0; i < list.childNodes.length; i++) {
+        var item = chatList.childNodes[i];
+        list.removeChild(item);
+    }
+};
+
+var populateList = function(list, members) {
+        
+    for(i=0; i < members.length; i++) {
+        var li = document.createElement("li");
+
+        var img = document.createElement("img");
+        img.setAttribute("src", "http://lorempixum.com/40/40/nature/1");
+
+        var h3 = document.createElement("h3");
+        h3.innerHTML = members[i];
+
+        var p = document.createElement("p");
+        p.innerHTML = "Hi";
+
+        li.appendChild(img);
+        li.appendChild(h3);
+        li.appendChild(p);
+        
+        list.appendChild(li);
+    }
+};
+
+var refreshList = function(list, members) {
+    clearList(list);
+    populateList(list, members);
+};
